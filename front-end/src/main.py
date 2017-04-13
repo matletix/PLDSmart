6606fe874328db8e07a95d7dd89f8eeba81a2f08
Peamcy <hugo.humbert@insa-lyon.fr>
2017-04-13 17:46:26 +0200

    Update main.py

--- front-end/src/main.py
+++ front-end/src/main.py
@@ -2,18 +2,22 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
+
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
+import mapview
 
 Builder.load_string("""
+#:import sys sys
+#:import MapSource mapview.MapSource
 <MenuScreen>:
     BoxLayout:
-    	orientation: 'vertical'
-    	padding: [10,50,10,50]
-    	spacing: 50
-    	Label:
-			text: "Feli'CITY"
-			font_size: 50
+        orientation: 'vertical'
+        padding: [10,50,10,50]
+        spacing: 50
+        Label:
+            text: "Feli'CITY"
+            font_size: 50
         Button:
             text: 'Se connecter'
             font_size: 26          	
@@ -21,19 +25,19 @@ Builder.load_string("""
             text: "S'inscrire"
             font_size: 26
         Button:
-        	text: "Visiter l'application"
-        	font_size: 26
-        	on_press:
-        		root.visit_Appli()
+            text: "Visiter l'application"
+            font_size: 26
+            on_press:
+                root.visit_Appli()
 
 <GameModeScreen>:
     BoxLayout:
-    	orientation: 'vertical'
-    	padding: [10,50,10,50]
-    	spacing: 50
-    	Label:
-    		text: "Choisis ton mode de jeu !"
-    		font_size: 34
+        orientation: 'vertical'
+        padding: [10,50,10,50]
+        spacing: 50
+        Label:
+            text: "Choisis ton mode de jeu !"
+            font_size: 34
         Button:
             text: "Mode détente"
             font_size: 26
@@ -46,17 +50,18 @@ Builder.load_string("""
             text: "Retour"
             font_size: 20
             on_press:
-            	root.back_to_menu()
+                root.back_to_menu()
+
 <ChillScreen>:
-	BoxLayout:
-		orientation: 'vertical'
-    	padding: [10,50,10,50]
-    	spacing: 30
-    	Label:
-    		text: "Mode détente"
-    		font_size: 34
-		Label:
-			text: "Choisis ton niveau !"
+    BoxLayout:
+        orientation: 'vertical'
+        padding: [10,50,10,50]
+        spacing: 30
+        Label:
+            text: "Mode détente"
+            font_size: 34
+        Label:
+            text: "Choisis ton niveau !"
             font_size: 30
         Button:
             text: "Niveau 1"
@@ -74,6 +79,7 @@ Builder.load_string("""
             font_size: 20
             on_press:
                 root.back_to_menu()
+
 <Level_1_Screen>
     BoxLayout:
         orientation: 'vertical'
@@ -88,13 +94,38 @@ Builder.load_string("""
         Button:
             text: "Parcours 1"
             font_size: 26
-            size: (200, 100)
+            on_press:
+                root.parcours_1()
         Button:
             text: "Retour"
             font_size: 20
             on_press:
                 root.back_to_menu()
 
+<Parcours_1_Screen>
+    
+    MapView:
+        lat: 45.75
+        lon: 4.85
+        zoom: 15
+        map_source: MapSource(sys.argv[1], attribution="") if len(sys.argv) > 1 else "osm"
+
+        MapMarkerPopup:
+            lat: 45.75
+            lon: 4.85
+            popup_size: dp(230), dp(130)
+
+            Bubble:
+                BoxLayout:
+                    orientation: "horizontal"
+                    padding: "5dp"
+                    AsyncImage:
+                        source: "http://www.cityzeum.com/images/lieu/bigstock-basilica-of-notre-dame-de-four-47809538.jpg"
+                        mipmap: True
+                    Label:
+                        text: "[b]Lyon[/b]\\n1 506 615 hab\\n5 10 583 hab./km2"
+                        markup: True
+                        halign: "center"
 
 """)
 
@@ -124,7 +155,12 @@ class Level_1_Screen(Screen):
     def back_to_menu(self):
         self.manager.current = 'chillScreen'
         self.manager.transition.direction = 'right'
+    def parcours_1(self):
+        self.manager.current = 'parcours_1'
+        self.manager.transition.direction = 'left'
 
+class Parcours_1_Screen(Screen):
+    pass
 
 # Creation du screenmanager
 sm = ScreenManager()
@@ -132,6 +168,7 @@ sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(GameModeScreen(name='gamemode'))
 sm.add_widget(ChillScreen(name='chillScreen'))
 sm.add_widget(Level_1_Screen(name= 'level_1'))
+sm.add_widget(Parcours_1_Screen(name= 'parcours_1'))
 
 class TestApp(App):
 
