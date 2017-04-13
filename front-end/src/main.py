ac7092e814aaa71717a4f44b1b7a3fbf3af89929
Peamcy <hugo.humbert@insa-lyon.fr>
2017-04-13 15:28:15 +0200

    Update main.py

--- front-end/src/main.py
+++ front-end/src/main.py
@@ -1,24 +1,49 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
 
 Builder.load_string("""
 <MenuScreen>:
     BoxLayout:
+    	orientation: 'vertical'
+    	padding: [10,50,10,50]
+    	spacing: 50
+    	Label:
+			text: "Feli'CITY"
+			font_size: 50
         Button:
-            text: 'Goto settings'
-            on_press:
-            	root.manager.current = 'settings'
+            text: 'Se connecter'
+            font_size: 26          	
+        Button:
+            text: "S'inscrire"
+            font_size: 26
+        Button:
+        	text: "Visiter l'application"
+        	font_size: 26
+        	on_press:
+        		root.manager.current = 'gamemode'
             	root.manager.transition.direction = 'left'
-        Button:
-            text: 'Quit'
 
-<SettingsScreen>:
+<GameModeScreen>:
     BoxLayout:
+    	orientation: 'vertical'
+    	padding: [10,50,10,50]
+    	spacing: 50
+    	Label:
+    		text: "Choisis ton mode de jeu !"
+    		font_size: 34
         Button:
-            text: 'My settings button'
+            text: "Mode détente"
+            font_size: 26
+        Button:
+            text: "Mode compétitif"
+            font_size: 26
         Button:
             text: 'Back to menu'
+            font_size: 26
             on_press:
             	root.manager.current = 'menu'
             	root.manager.transition.direction ='right'
@@ -29,13 +54,13 @@ Builder.load_string("""
 class MenuScreen(Screen):
     pass
 
-class SettingsScreen(Screen):
+class GameModeScreen(Screen):
     pass
 
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
-sm.add_widget(SettingsScreen(name='settings'))
+sm.add_widget(GameModeScreen(name='gamemode'))
 
 class TestApp(App):
 
