a447b1e2da632226d5d84f331a1d84470130c244
Nathan-k- <nathan.kienlen@gmail.com>
2017-04-22 20:41:37 +0200

    update db

--- back-end/Docker/insert_datas.sql
+++ back-end/Docker/insert_datas.sql
@@ -3,9 +3,9 @@ INSERT INTO user_data VALUES ('tata', 25, 50, 'F', 'tata@insa-lyon.fr', 'mdp');
 INSERT INTO centers_of_interest(id, id_sitra1, type, type_detail, nom, adresse, codepostal, commune, telephone, email, siteweb, ouverture, tarifsenclair, tarifsmin, tarifsmax, date_creation, last_update, last_update_fme, coordinates)
 VALUES ( '8045' ,  'undifined' ,  'PATRIMOINE_CULTUREL' ,  '' ,  'l''espace nature' ,  'Espace nature Aquaria' ,  '69660' ,  'Collonges-au-Mon' ,  '0478220212' ,  '' ,  'http://www.collongesaumontdor.fr' ,  '' ,  'Gratuit' ,  0 ,  0 ,  '2011-03-11 15:05:18' ,  '2016-04-20 06:22:11' ,  '2017-03-16 00:15:34' , ST_GeomFromGeoJSON ( '{"type":"Point","coordinates":[ 4.834414, 45.82862]}' ));
 
-INSERT INTO public.course (id_level, id_course, theme, story_course) VALUES (1, 1, 'lvl1:1', 'Il était une fois');
-INSERT INTO public.course (id_level, id_course, theme, story_course) VALUES (1, 2, 'lvl1:2', 'lorem');
-INSERT INTO public.course (id_level, id_course, theme, story_course) VALUES (2, 3, 'lvl2:1', 'ipsum');
+INSERT INTO public.course (level, id_course, theme, story_course) VALUES (1, 1, 'lvl1:1', 'Il était une fois');
+INSERT INTO public.course (level, id_course, theme, story_course) VALUES (1, 2, 'lvl1:2', 'lorem');
+INSERT INTO public.course (level, id_course, theme, story_course) VALUES (2, 3, 'lvl2:1', 'ipsum');
 
 INSERT INTO public.course_ci (id_ci, level, id_course, qr_code, story_ci, ordre) VALUES (2996254, 1, 1, 'coucou', 'debut', 1);
 INSERT INTO public.course_ci (id_ci, level, id_course, qr_code, story_ci, ordre) VALUES (132234, 1, 2, '1:2', 'st1:2', 1);
