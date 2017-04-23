3f7c92206e6219b987b2b887d951a1d34dc177df
Nathan-k- <nathan.kienlen@gmail.com>
2017-04-23 10:04:47 +0200

    COI class, with to geoJson

--- back-end/nodejs/OO_Coi.js
+++ back-end/nodejs/OO_Coi.js
@@ -2,11 +2,24 @@
  Classe pour representer un centre d'interet
  */
 
-class Coi {
-    contructor(id, nom, lat, long) {
+
+var GeoJson = require('geojson');
+
+class OO_Coi {
+    constructor(id, nom, lat, long) {
         this.id = id;
         this.nom = nom;
         this.lat = lat;
-        this.long = long;
+        this.lng = long;
+    }
+
+
+    toMyGeoJson() {
+        let output =  GeoJson.parse(this, {Point: ['lat', 'lng']});
+        //console.log(this.id);
+        //console.log(output);
+        return output;
     }
 }
+
+module.exports = OO_Coi;
