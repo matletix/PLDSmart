08207253563e2bb25685c37bbca314c884d1da85
nathan-K- <nathan.kienlen@gmail.com>
2017-05-03 17:03:10 +0200

    Update insert_datas.sql

--- back-end/Docker/insert_datas.sql
+++ back-end/Docker/insert_datas.sql
@@ -53,10 +53,10 @@ VALUES ( '25' ,  'undifined' ,  'PATRIMOINE_CULTUREL' ,  '' ,  'Département Inf
 INSERT INTO centers_of_interest(id, id_sitra1, type, type_detail, nom, adresse, codepostal, commune, telephone, email, siteweb, ouverture, tarifsenclair, tarifsmin, tarifsmax, date_creation, last_update, last_update_fme, coordinates)
 VALUES ( '26' ,  'undifined' ,  'PATRIMOINE_CULTUREL' ,  '' ,  'Double Mixte' ,  '19 Avenue Gaston Berger' ,  '69100' ,  'Villeurbanne' ,  '' ,  '' ,  '' ,  '' ,  'Gratuit' ,  0 ,  0 ,  '2011-03-11 15:05:18' ,  '2016-04-20 06:22:11' ,  '2017-03-16 00:15:34' , ST_GeomFromGeoJSON ( '{"type":"Point","coordinates":[ 4.87261280000007, 45.7807042]}' ));
 
-INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 1, 'lvl1:1', 'Il était une fois Lyon');
-INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 2, 'lvl1:2', 'lorem');
-INSERT INTO public.course (level, id_course, theme, description) VALUES (2, 3, 'lvl2:1', 'ipsum');
-INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 4, 'lvl2:1', 'A la découverte de campus');
+INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 1, 'Lyon historique', 'Il était une fois Lyon');
+INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 2, 'Un parcours', 'lorem');
+INSERT INTO public.course (level, id_course, theme, description) VALUES (2, 3, 'Encore un parcours', 'ipsum');
+INSERT INTO public.course (level, id_course, theme, description) VALUES (1, 4, 'L INSA de A a Z', 'A la découverte de campus');
 
 INSERT INTO public.course_coi (id_coi, level, id_course, qr_code, description, position_in_course) VALUES (1, 1, 2, 'coucou', 'debut', 1);
 INSERT INTO public.course_coi (id_coi, level, id_course, qr_code, description, position_in_course) VALUES (2, 1, 2, '1:2', 'st1:2', 1);
